49081a02c51666dde78d108d97c11f026b76566c
Umair Ahmad <ahmadumair17@gmail.com>
2017-11-24 06:11:49 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,7 +1,7 @@
 describe "Not having any errors and being all green" do
 
   context 'NameError' do
-    it 'raises a NameError when encountering undefined barewords' then
+    it 'raises a NameError when encountering undefined barewords'
       expect{
         load './lib/a_name_error.rb'
       }.to_not raise_error
@@ -9,7 +9,7 @@ describe "Not having any errors and being all green" do
   end
 
   context 'SyntaxError' do
-    it 'raises a SyntaxError for nonsensical code' then
+    it 'raises a SyntaxError for nonsensical code'
       expect{
         load './lib/a_syntax_error.rb'
       }.to_not raise_error
@@ -17,7 +17,7 @@ describe "Not having any errors and being all green" do
   end
 
   context 'TypeError' do
-    it 'raises a TypeError for objects of the wrong type' then
+    it 'raises a TypeError for objects of the wrong type'
       expect{
         load './lib/a_type_error.rb'
       }.to_not raise_error
@@ -25,7 +25,7 @@ describe "Not having any errors and being all green" do
   end
 
   context 'ZeroDivisionError' do
-    it 'raises a ZeroDivisionError for dividing by zero' then
+    it 'raises a ZeroDivisionError for dividing by zero'
       expect{
         load './lib/a_division_by_zero_error.rb'
       }.to_not raise_error
