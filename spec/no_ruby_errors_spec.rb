c6a5474a96f405c8f22123206030f2a8261806fb
Umair Ahmad <ahmadumair17@gmail.com>
2017-11-24 06:13:23 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -6,7 +6,7 @@ describe "Not having any errors and being all green" do
         load './lib/a_name_error.rb'
       }.to_not raise_error
     end
-  end
+
 
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code'
@@ -14,7 +14,7 @@ describe "Not having any errors and being all green" do
         load './lib/a_syntax_error.rb'
       }.to_not raise_error
     end
-  end
+
 
   context 'TypeError' do
     it 'raises a TypeError for objects of the wrong type'
@@ -22,7 +22,7 @@ describe "Not having any errors and being all green" do
         load './lib/a_type_error.rb'
       }.to_not raise_error
     end
-  end
+
 
   context 'ZeroDivisionError' do
     it 'raises a ZeroDivisionError for dividing by zero'
@@ -30,5 +30,5 @@ describe "Not having any errors and being all green" do
         load './lib/a_division_by_zero_error.rb'
       }.to_not raise_error
     end
-  end
+
 end
